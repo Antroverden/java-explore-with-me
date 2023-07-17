4bb5b15db10f14efa41e190566484b529bc561d2
Antroverden <Antroverden@yandex.ru>
2023-07-18 01:28:12 +0300

    feature: add custom exceptions

---
+++ main-service/src/main/java/ru/practicum/mainservice/exception/ErrorHandler.java
@@ -0,0 +1,28 @@
+package ru.practicum.mainservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse userNotFoundExc(final BadRequestException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse userNotFoundExc(final NotFoundException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse userNotFoundExc(final ConflictException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+}
---
+++ main-service/src/main/java/ru/practicum/mainservice/exception/ErrorResponse.java
@@ -0,0 +1,11 @@
+package ru.practicum.mainservice.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ErrorResponse {
+
+    private final String error;
+}
