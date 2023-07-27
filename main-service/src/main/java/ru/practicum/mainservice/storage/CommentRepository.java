df4a10b72ab55492d053de4b0da7a9596eb48f31
Antroverden <Antroverden@yandex.ru>
2023-07-28 02:13:28 +0300

    feature: add comments to events

--- main-service/src/main/java/ru/practicum/mainservice/storage/CommentRepository.java
+++ main-service/src/main/java/ru/practicum/mainservice/storage/CommentRepository.java
@@ -5,14 +5,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.mainservice.entity.Comment;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
     Page<Comment> findAllByEvent_Id(Integer eventId, Pageable pageable);
+
     Optional<Comment> deleteByIdAndEvent_Id(Integer commentId, Integer eventId);
+
     Optional<Comment> deleteByIdAndEvent_IdAndAuthor_Id(Integer commentId, Integer eventId, Integer authorId);
+
     Optional<Comment> findByIdAndEvent_IdAndAuthor_Id(Integer commentId, Integer eventId, Integer authorId);
+
     Optional<Comment> findByIdAndEvent_Id(Integer commentId, Integer eventId);
 }
