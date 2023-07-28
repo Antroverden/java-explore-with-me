bc42c7dce5026d2b5ef125a29eada9d7921b876a
Antroverden <Antroverden@yandex.ru>
2023-07-28 12:05:29 +0300

    feature: add postman tests

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # java-explore-with-me
-Template repository for ExploreWithMe project.
+https://github.com/Antroverden/java-explore-with-me/pull/6
--- stats/client/src/main/java/ru/practicum/stats/client/StatsClient.java
+++ stats/client/src/main/java/ru/practicum/stats/client/StatsClient.java
@@ -1,9 +1,11 @@
 package ru.practicum.stats.client;
 
 import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import ru.practicum.stats.dto.EndpointHitDto;
 
+@Component
 public class StatsClient {
 
     private final RestTemplate restTemplate = new RestTemplate();
